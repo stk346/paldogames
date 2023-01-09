e340491777f0f79f25b964966f76bad74c0ee91a
stk346 <stk346@gamil.com>
2023-01-09 20:55:50 +0900

    init

---
+++ src/main/java/hello/paldogames/PaldogamesApplication.java
@@ -0,0 +1,13 @@
+package hello.paldogames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaldogamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaldogamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/paldogames/PaldogamesApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.paldogames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaldogamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
